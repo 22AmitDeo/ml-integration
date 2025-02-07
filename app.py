70a868cd2041614eb4391c888fea9570a92a73fa
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-02-07 22:53:36 +0530

    #

--- app.py
+++ app.py
@@ -4,3 +4,20 @@ import pickle
 
 app= Flask(__name__)
 model=pickle.load(open("model.pkl","rb"))
+
+@app.route("/")
+def Home():
+    return render_template("index.html")
+
+@app.route("/predict",methods=["POST"])
+
+def predict():
+    float_feature=[float(x) for x in request.form.values()]
+    features=[np.array(float_feature)] 
+    prediction=model.predict(features)
+    return render_template("index.html",prediction_test="Predicted Crop is {}".format(prediction))
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
